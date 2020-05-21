4f1a54a20ad7d0f1eaf4d9c232c1787ba33c7feb
Oiapokxui <enrique.emanuel@usp.br>
2020-05-21 01:01:27 -0300

    Adicionando Main.java, estável. Enfim concluída a segunda etapa do projeto.

--- Hyperskill/My_1st_ML_Algo/Main.java
+++ Hyperskill/My_1st_ML_Algo/Main.java
@@ -1,103 +1,213 @@
 package recognition;
 import java.util.*;
 
+/*
+ *				RECOGNITION.
+ *	Projeto do curso Hyperskill.
+ *	Visa projetar uma rede neural do tipo perceptron
+ *	a fim de identificar digitos em uma grid 3x3
+ *	 
+ *	São recebidas, por input do usuario, 5 linhas que 
+ *	contem alguma permutacao de 3 'X' ou '_', essas 5 
+ *	linhas, representam o desenh de um digito,
+ *	onde o carac. 'X' representaria um pixel
+ *	aceso e o caract '_' um pixel apagado. 
+ *
+ *	Apos o processamento, espera-se que a rede saiba identificar
+ *	qual o digito desenhado, imprimindo no terminal o inteiro que
+ *	corresponde ao dígito.
+ *
+ */
+ 
 public class Main {
 	public static void main(String[] args) {
-		// redeNeural é um arranjo de vetores.
-		// Dá um atalho para os atalhos dos arranjos
-		// dos neuronios i da rede. Onde i eh 
-		// o indice da linha.
-		int[][][] redeNeural = new int[2][15][1];
-		int[][] neuronA0 = new int[15][1];
-		redeNeural[0] = neuronA0;
+		int[] neuronA0 = new int[15];
 		// Estes sao os valores de bias para os neuronios
-		// respectivos aos dígitos
-		// 1, 2, 3, 4, 5, 6, 7, 8, 9, 0. 
-		int[][] bias = {
-				{6},
-				{1},
-				{0},
-				{2},
-				{0},
-				{-1},
-				{3},
-				{-2},
-				{-1},
-				{-1}
+		// respectivos aos dígitos  do 1 ao 9, e 0
+		int[] bias = {
+				 6,	// 1
+				 1,	// 2
+				 0,	// 3
+				 2,	// 4
+				 0,	// 5
+				-1,	// 6
+				 3,	// 7
+				-2,	// 8
+				-1,	// 9
+				-1	// 0
 			};
 		 
-		Scanner scan = new Scanner(System.in);
 		System.out.println("Input grid: ");
+		 	
+		// Cada linha dessa matriz representa os pesos
+		// do n-esimo neuronio da camada 1.
+		// pesos é um int[10][15] onde cada linha
+		// representa os 15 pesos para um digito ideal
+		int[][] pesos = 
+		{
+				{	// Um
+					 -1,+1,-1,
+					 -1,+1,-1,
+					 -1,+1,-1,
+					 -1,+1,-1,
+					 -1,+1,-1},
+				{	// Dois
+					 +1,+1,+1,
+					 -1,-1,+1,
+					 +1,+1,+1,
+					 +1,-1,-1,
+					 +1,+1,+1},
+				{	// Tres
+					 +1,+1,+1,
+					 -1,-1,+1,	
+					 +1,+1,+1,
+					 -1,-1,+1,
+					 +1,+1,+1},
+				{	// Quatro
+					 +1,-1,+1,
+					 +1,-1,+1,
+					 +1,+1,+1,
+					 -1,-1,+1,
+					 -1,-1,+1},
+				{ 	// Cinco
+					 +1,+1,+1,
+					 +1,-1,-1,
+					 +1,+1,+1,
+					 -1,-1,+1,
+					 +1,+1,+1},
+				{	// Seis
+					 +1,+1,+1,
+					 +1,-1,-1,
+					 +1,+1,+1,
+					 +1,-1,+1,
+					 +1,+1,+1},
+				{	// Sete
+					 +1,+1,+1,
+					 -1,-1,+1,
+					 -1,-1,+1,
+					 -1,-1,+1,
+					 -1,-1,+1},
+				{	// Oito
+					 +1,+1,+1,
+					 +1,-1,+1,	
+					 +1,+1,+1,
+					 +1,-1,+1,
+					 +1,+1,+1},
+				{ 	// Nove
+					 +1,+1,+1,
+					 +1,-1,+1,
+					 +1,+1,+1,
+					 -1,-1,+1,
+					 +1,+1,+1},
+				{	// Zero
+					 +1,+1,+1,
+					 +1,-1,+1,
+					 +1,-1,+1,
+					 +1,-1,+1,
+					 +1,+1,+1}
+		} ;
+
+		neuronA0 = preencheNeuronA0 (neuronA0);
+		int[] neuronA1 = novaCamada (pesos, neuronA0, bias);	 
+		 
+		System.out.println("This number is: " + chosenNeuron(neuronA1));
+	}
+	 
+	/*
+	 *	Recebe o uma camada de neuronios e preenche ela
+	 *	de acordo com inputs a serem digitados pelo
+	 *	usuario.
+	 *	
+	 *	Entradas:
+	 *		- neuronA0 : Um arranjo de inteiros que
+	 *		representa uma camada de neuronios.
+	 *	Saidas:
+	 *		- neuronA0 : O mesmo arranjo só que
+	 *		preenchido.
+	 */
+	static int[] preencheNeuronA0 (int[] neuronA0){
+		Scanner scan = new Scanner(System.in);
 		String[] linha = new String[5];
 		int h = 0; 
 		for (int i = h; i < 5; i++) {
 			linha[i] = scan.nextLine();
 			for (int v = 0; v < 3; v++){ 
-				neuronA0[h][0] = (linha[i].charAt(v) == 'X')?
-					1 : -1;
+				neuronA0[h] = (linha[i].charAt(v) == 'X')?
+					1 : 0;
 				h++;
 			}
 		}
-		 	
-		// Cada linha dessa matriz representa os pesos
-		// do n-esimo neuronio da camada 1.
-		// pesos é um int[10][15] e estão distribuidos
-		// da seguinte maneira, de acordo com a linha:
-		// 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-		int[][] pesos = {
-			{-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1},
-			{1,1,1,-1,-1,1,1,1,1,1,-1,-1,1,1,1},
-			{1,1,1,-1,-1,1,1,1,1,-1,-1,1,1,1,1},
-			{1,-1,1,1,-1,1,1,1,1,-1,-1,1,-1,-1,1},
-			{1,1,1,1,-1,-1,1,1,1,-1,-1,1,1,1,1},
-			{1,1,1,1,-1,-1,1,1,1,1,-1,1,1,1,1},
-			{1,1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1},
-			{1,1,1,1,-1,1,1,1,1,-1,-1,1,1,1,1},
-			{1,1,1,1,-1,1,1,1,1,-1,-1,1,1,1,1},
-			{1,1,1,1,-1,1,1,-1,1,1,-1,1,1,1,1}
-		} ;
-		int[][] neuronA1 = multiMatriz (pesos, neuronA0);	 
-		somaMatriz (neuronA1, bias);
-		 
-		System.out.println();
-		System.out.println("This number is: " + chosenNeuron(neuronA1));
+		return neuronA0;
 	}
 	 
-	static int[][] multiMatriz(int[][] m1, int[][] m2){
-		int[][] Resultante = new int[m1.length][m2[0].length];
-		if (m1[0].length != m2.length){
+	/*
+	 *	Realiza operacoes necessarias para se obter uma
+	 *	nova camada de neuronios, a partir de uma matriz
+	 *	de pesos, do vetor da camada anterior de neuronios
+	 *	e do vetor dos pesos.
+	 *
+	 *	Entrada:
+	 *		- pesos : Matriz que representa todos os pesos
+	 *		para o calculo.
+	 *		- neuron : Arranjo que representa a camada anterior
+	 *		de neuronios a ser transformada.
+	 *		- bias : Arranjo que armazena o valor de todos os vies
+	 *		necessarios.
+	 *	Saida:
+	 *		- resultante : Uma nova camada de neuronios.
+	 */
+	static int[] novaCamada (int[][] pesos, int[] neuron, int[] bias) {
+		// MULTIPLICAO ENTRE PESOS E NEURON
+		int[] resultante = new int[pesos.length];
+		if (pesos[0].length != neuron.length){
 			return null;
 		}
-		int linhasR = m1.length;
-		int colunasR = m2[0].length;
-		int colunasM1 = m1[0].length;
+		int linhasR = pesos.length;
+		int colunasR = 1;
+		int colunasM1 = pesos[0].length;
 		for (int i = 0; i < linhasR ; i++){
 			for (int v = 0; v < colunasR ; v++){
-				Resultante[i][v] = 0;
+				resultante[i] = 0;
 				for(int w = 0; w < colunasM1; w++){
-					Resultante[i][v] += m1[i][w] * m2[w][v];
+					resultante[i] += pesos[i][w] * neuron[w];
 				} 
 			}
 		}
-		return Resultante;
+		 
+		// ADICAO DOS VIESES.
+		somaNeuronioPeso (resultante, bias);
+		return resultante;
 	}
-
-	static void somaMatriz(int[][] m1, int[][] m2){
-		for (int i = 0; i < m1.length; i++){
-			for (int v = 0; v < m1[0].length ; v++){
-				m1[i][v] += m2[i][v];
-			}
+	
+	/*
+	 *	Adiciona os vies a uma camada existente de neuronios
+	 *
+	 *	Entrada:
+	 *		- vetor : Arranjo que representa a 
+	 *		camada de neuronio existente
+	 *		- bias : O arranjo de vieses.
+	 *	Saida:
+	 *		nao ha.
+	 */
+	static void somaNeuronioPeso (int[] vetor, int[] bias) {
+		for (int i = 0; i < vetor.length; i++){
+			vetor[i] += bias[i];
 		}
 	}
 	 
-	static int chosenNeuron (int[][] n){
+	/*
+	 *	Dentro da camada, escolhe o neuronio de valor maximo
+	 *	
+	 *	Entrada:
+	 *		- neuron : Arranjo de inteiros que representa a
+	 *		camada de neuronios.
+	 *	Saida
+	 *		nao ha.
+	 */
+	static int chosenNeuron (int[] neuron) {
 		int maxInd = 0;
-		for (int i = 0; i < n.length ; i++){
-			for (int v = 0; v < n[0].length ; v++){
-				System.out.printf("Numero: %d", (i+1)%10);
-				System.out.printf(" Ativacao: %d\n", n[i][0]);
-				if (n[maxInd][0] < n[i][0]) maxInd = i;
-			}
+		for (int i = 0; i < neuron.length ; i++){
+			if (neuron[maxInd] < neuron[i]) maxInd = i;
 		}
 		return (maxInd + 1)% 10;
 	}
